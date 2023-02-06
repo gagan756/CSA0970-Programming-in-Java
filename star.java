eb843b7de15c2eeb62fd7670fdd164bdb675c50a
Gagankumar <114555082+gagan756@users.noreply.github.com>
2023-02-06 08:59:41 +0530

    Add files via upload

--- star.java
+++ star.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
-public class star {
- public static void main(String[] args) {
- Scanner sc=new Scanner(System.in);
- int n = sc.nextInt();
- int a, b;
- for (a = 0; a < n; a++) {
- for (b = 1; b <= a; b++) {
- System.out.print(" "+b+" ");}
- System.out.println();}
- }
+public class Star {
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of rows: ");
+int rows = sc.nextInt();
+for(int i = 0; i <= rows - 1; i++){ 
+for(int j = 0; j <= i; j++){
+System.out.print("1" + " ");
+}
+System.out.println("");
+}
+for (int i = rows - 1; i >= 0; i--){
+for (int j = 0; j <= i - 1; j++){
+System.out.print("1" + " ");
+}
+System.out.println("");
+}
+sc.close();
+}
 }
